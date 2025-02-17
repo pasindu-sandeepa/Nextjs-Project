dc2c18330bdda616091482babe8458d95a2509ea
pasindu-sandeepa <pasindusandeep23@gmail.com>
2025-02-18 00:52:55 +0530

    connect mongodb database

--- app/api/v1/movies/route.js
+++ app/api/v1/movies/route.js
@@ -1,45 +1,26 @@
 import { NextResponse } from "next/server";
-
-const MOVIES = [
-    {
-        id: 1,
-        title: "The Shawshank Redemption",
-        year: 1994,
-        rating: 9.3,
-        genres: ["Drama"],
-    },
-    {
-        id: 2,
-        title: "The Godfather",
-        year: 1972,
-        rating: 9.2,
-        genres: ["Crime", "Drama"],
-    },
-    {
-        id: 3,
-        title: "The Dark Knight",
-        year: 2008,
-        rating: 9.0,
-        genres: ["Action", "Crime", "Drama"],
-    },
-    {
-        id: 1,
-        title: "Mafia",
-        year: 1997,
-        rating: 9.8,
-        genres: ["Action", "Crime", "Drama"],
-    },
-      {
-        id: 5,
-        title: "venem",
-        year: 1997,
-        rating: 9.8,
-        genres: ["Action", "Crime", "Drama"],
-    },
-    
-];
-    
+import clientPromise from "../../../libs/mongodb";
 
 export const GET = async (req) => {
-    return NextResponse.json({ sucess:true,movies: MOVIES });
-    }
+  // Get movies from the mongoDB
+  try {
+    const client = await clientPromise();
+
+    const database = client.db("sample_mflix");
+
+    const movies = await database
+      .collection("movies")
+      .find({})
+      .sort({ metacritic: -1 })
+      .limit(10)
+      .toArray();
+
+    return NextResponse.json(movies);
+  } catch (error) {
+    console.log("MongoDB Error", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+};
--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,4 +1,12 @@
+
 import { getMovies } from "../libs/apis/server";
+import {   
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle, } from "shadcn"; // Ensure the correct import path
 
 export default async function DashboardPage() {
   // 1.add shadcn card
@@ -6,7 +14,8 @@ export default async function DashboardPage() {
   // 3. read the dummy response
   // 4. render dataset in ui
 
-  const { movies } = await getMovies();
+  const moviesQuery = await getMovies();
+  console.log("movie", moviesQuery);
 
   return (
     <main>
@@ -18,11 +27,15 @@ export default async function DashboardPage() {
       </div>
 
       <div className="container mt-8">
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  gap-4">
-          {movies?.length &&
-            movies.map((movie) => (
-              <div key={movie.id} className="h-96 bg-blue-400">
-                {movie.title}
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+          {moviesQuery?.length &&
+            moviesQuery.map((movie) => (
+              <div key={movie?._id} className="h-96 bg-blue-400">
+                {movie?.id} 
+                 <Card>
+                 <CardTitle>{movie?.title}</CardTitle>
+                 <CardDescription>{movie?.plot}</CardDescription>
+                 </Card>
               </div>
             ))}
         </div>
